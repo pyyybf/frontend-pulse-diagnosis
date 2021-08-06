657fe64f9da7d67a2d3f6b3b6fb3c54b7fa83d18
PY <181250111@smail.nju.edu.cn>
2021-08-06 23:04:32 +0800

    新增app模块中设置query

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -4,7 +4,6 @@ const app = {
   },
   mutations: {
     set_menuList: (state, data) => {
-      // console.log('set_menuList')
       state.menuList = []
       data.forEach(permission => {
         if (permission === '文献管理') {
@@ -15,16 +14,19 @@ const app = {
                 title: '古籍管理',
                 index: '/admin/ancientBookManage',
                 position: '',
+                query: {},
               },
               {
                 title: '现代文献管理',
                 index: '/admin/modernTextManage',
                 position: '',
+                query: {},
               },
               {
                 title: '结构化文本管理',
                 index: '/admin/structuredTextManage',
                 position: '',
+                query: {},
               },
             ]
           })
@@ -33,30 +35,80 @@ const app = {
             title: '用户管理',
             index: '/admin/userManage',
             position: '',
+            query: {},
           })
         } else if (permission === '角色权限管理') {
           state.menuList.push({
             title: '角色权限管理',
             index: '/admin/permissionManage',
             position: '',
+            query: {},
           })
         } else if (permission === '脉图管理') {
           state.menuList.push({
             title: '脉图管理',
             index: '/admin/diagramManage',
             position: '',
+            query: {},
           })
         } else if (permission === '术语管理') {
           state.menuList.push({
             title: '术语管理',
             index: '/admin/termManage',
             position: '',
+            query: {},
           })
         } else if (permission === '题库管理') {
           state.menuList.push({
             title: '题库管理',
             index: '/admin/questionManage',
             position: '',
+            query: {},
+          })
+        } else if (permission === '文本阅读') {
+          state.menuList.push({
+            title: '文本阅读',
+            children: [
+              {
+                title: '古籍阅读',
+                index: '/user/ancientBook',
+                position: '',
+                query: {},
+              },
+              {
+                title: '现代文献阅读',
+                index: '/user/modernText',
+                position: '',
+                query: {},
+              },
+              {
+                title: '结构文本阅读',
+                index: '/user/structuredText',
+                position: '',
+                query: {},
+              },
+            ]
+          })
+        } else if (permission === '脉图分类') {
+          state.menuList.push({
+            title: '脉图分类',
+            index: '/user/diagram',
+            position: '',
+            query: {},
+          })
+        } else if (permission === '段落分析') {
+          state.menuList.push({
+            title: '段落分析',
+            index: '/user/term',
+            position: '',
+            query: {},
+          })
+        } else if (permission === '在线练习') {
+          state.menuList.push({
+            title: '在线练习',
+            index: '/user/question',
+            position: '',
+            query: {},
           })
         }
       })
@@ -64,18 +116,18 @@ const app = {
       // console.log(state.menuList)
     },
     set_singleMenuItem: (state, data) => {
-      // console.log('set_singleMenuItem data =', data)
+      console.log('set_singleMenuItem data =', data)
       for (let i = 0; i < state.menuList.length; i++) {
-        // console.log(state.menuList[i].children)
         if (state.menuList[i].children) {
-          // alert('11111111111111')
           for (let j = 0; j < state.menuList[i].children.length; j++) {
-            if (data.startsWith(state.menuList[i].children[j].index)) {
-              state.menuList[i].children[j].position = data.split('/')[3] ? data.split('/')[3] : '';
+            if (data.path.startsWith(state.menuList[i].children[j].index)) {
+              state.menuList[i].children[j].position = data.path.split('/')[3] ? data.path.split('/')[3] : '';
+              state.menuList[i].children[j].query = data.query;
             }
           }
-        } else if (data.startsWith(state.menuList[i].index)) {
-          state.menuList[i].position = data.split('/')[3] ? data.split('/')[3] : '';
+        } else if (data.path.startsWith(state.menuList[i].index)) {
+          state.menuList[i].position = data.path.split('/')[3] ? data.path.split('/')[3] : '';
+          state.menuList[i].query = data.query;
         }
       }
       // console.log(state.menuList)
