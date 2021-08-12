1744900c56b263cf0e741b8bdce98e4c20ad0423
PY <181250111@smail.nju.edu.cn>
2021-08-12 14:25:20 +0800

    增加超时时间到30秒

--- src/axios/api.request.js
+++ src/axios/api.request.js
@@ -4,7 +4,7 @@ const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8001'
 
 const HttpRequest = axios.create({
   baseURL: baseUrl,  // api的base_url
-  timeout: 5000  // 请求超时时间
+  timeout: 30000  // 请求超时时间
 });
 
 export default HttpRequest;
